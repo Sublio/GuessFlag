3a404e16a7496e017ae39670b0ec129f6b8b3799
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2022-05-01 19:07:38 +0100

    Init commit

---
+++ GuessFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessFlag
+//
+//  Created by Denis Mordvinov on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
